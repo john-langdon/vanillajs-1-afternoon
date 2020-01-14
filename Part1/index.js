8bbb16a57f8e4d90c04ff6352783ea4d7a7eaa92
John Langdon <johnlangdon@Johns-MacBook-Pro.local>
2020-01-14 13:49:35 -0600

    commit

---
+++ Part1/index.js
@@ -0,0 +1,24 @@
+console.log(document);
+
+var arr []
+
+function play(getElementID){
+    const playerSpan = document.getElementById('player');
+if (playerSpan.innerText === 'X') {
+    playerSpan.innerText = 'O';
+} else {
+    playerSpan.innerText = "X";
+}
+}
+function play(clickedId){
+    const playerSpan = document.getElementById('player');
+    const clickedElement = document.getElementById(clickedId);
+
+    if (playerSpan.innerText === 'X') {
+        playerSpan.innerText = '0';
+        clickedElement.innerText = 'X';
+    } else {
+        playerSpan.innerText = 'X';
+        clickedElement.innerText = '0';
+    }
+}
